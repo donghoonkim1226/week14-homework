29599a2f9da664aa2fdb06b7aff1c157007af437
dong hoon kim <donghoonkim1226@gmail.com>
2016-02-12 12:23:49 -0500

    Set up the DB and also the APP

---
+++ burger/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var expressHandleBars = require("express-handlebars");
+var connect = require('connect');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+app.engine('handlebars', expressHandleBars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	database: "burgers_db"
+});
+
+app.listen(PORT, function() {
+  console.log("Listening on port %s", PORT);
+});

23d742def8bf4a32849ffc8d1b81dc6fa680654f
dong hoon kim <donghoonkim1226@gmail.com>
2016-02-13 23:23:00 -0500

    added the addBurger function into orm.js

--- burger/config/orm.js
+++ burger/config/orm.js
@@ -1,13 +1,20 @@
 var connection = require('../config/connection.js');
 
 var orm = {
-    selectAll: function(tableInput, cb) {
-        var s = 'SELECT * FROM ' + tableInput + ';';
-        connection.query(s, function(err, result) {
-            if (err) throw err;
-            cb(result);
-        });
-    }
+  selectAll: function(tableInput, cb) {
+    var s = 'SELECT * FROM ' + tableInput + ';';
+    connection.query(s, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  addBurger: function(burgerName, cb) {
+  	var a = 'INSERT INTO burger (burger_name) ' + burgerName + ';';
+  	connection.query(a, function(err, result) {
+  		if (err) throw err;
+  		cb(result);
+  	});
+  }
 };
     
 module.exports = orm;

1cbf0ff027a0116ff8851c264e780e53f7debbd0
dong hoon kim <donghoonkim1226@gmail.com>
2016-02-13 23:49:30 -0500

    added app.use to select public folder and also declared the routes to add and devourer

--- burger/server.js
+++ burger/server.js
@@ -6,11 +6,19 @@ var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 
+app.use(express.static(__dirname + '/public'));
+
 app.engine('handlebars', expressHandleBars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+app.use('/add', routes);
+app.use('/devourer', routes);
+
+var port = 8080;
 app.listen(PORT, function() {
   console.log("Listening on port %s", PORT);
 });

df6b700a75f7ae639dbb1d4da4dd65ec9de1b1b7
dong hoon kim <donghoonkim1226@gmail.com>
2016-02-15 12:32:07 -0500

    added the addBurger and devourer  function into orm.js linked it back to burger.js

--- burger/config/orm.js
+++ burger/config/orm.js
@@ -2,18 +2,25 @@ var connection = require('../config/connection.js');
 
 var orm = {
   selectAll: function(tableInput, cb) {
-    var s = 'SELECT * FROM ' + tableInput + ';';
+    var query = 'SELECT * FROM ' + tableInput + ';';
     connection.query(s, function(err, result) {
       if (err) throw err;
       cb(result);
     });
   },
-  addBurger: function(burgerName, cb) {
-  	var a = 'INSERT INTO burger (burger_name) ' + burgerName + ';';
-  	connection.query(a, function(err, result) {
-  		if (err) throw err;
-  		cb(result);
+  addBurger: function(tableInput, burgerName, cb) {
+    var query = "INSERT INTO " + tableInput + " (burger_name) VALUES (?)";
+    connection.query(query, [burgerName], function(err, result) {
+      if (err) throw err;
+      cb(result);
   	});
+  },
+  devourerBurger: function(tableInput, idInput, cb) {
+    var query = "UPDATE " + tableInput + " SET devoured = 1  WHERE id = ?";
+    connection.query(query, [idInput], function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
   }
 };
     
--- burger/models/burger.js
+++ burger/models/burger.js
@@ -1,11 +1,17 @@
 var orm = require('../config/orm.js');
 
-var burger = {
-	findAllburger: function(cb) {
-		orm.selectAll('burger', function(res){
-			cb(res)
-		});
+var burgers = {
+	findAll: function(cb) {
+	  orm.selectAll("burger", function(res){
+	    cb(res);
+	  });
 	}
+  addBurger: function(burgerName, cb){
+    orm.addBurger("burger", burgerName, cb);
+  },
+  devourBurger: function(idInput, cb){
+    orm.devourBurger("burger", idInput, cb);
+  },
 };
     
 module.exports = burger;
